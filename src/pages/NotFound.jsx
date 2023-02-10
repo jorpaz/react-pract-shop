ed5882670b63199452e27958c574269b1c314f8c
jorpaz <giovanny.paz@outlook.com>
2023-02-10 01:44:24 -0600

    Error 404 config

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from "react";
+import "../styles/NotFound.scss";
 
 const NotFound = () => {
   return (
-    <div><h1> NO ENCONTRADO </h1>
+    <div>
+      <div id="clouds">
+        <div className="cloud x1" />
+        <div className="cloud x1_5" />
+        <div className="cloud x2" />
+        <div className="cloud x3" />
+        <div className="cloud x4" />
+        <div className="cloud x5" />
+      </div>
+      <div className="c">
+        <div className="_404">404</div>
+        <hr />
+        <div className="_1">ESTA PAGINA</div>
+        <div className="_2">NO FUE ENCONTRADA</div>
+        <a className="btn" href="/">
+          IR AL INICIO
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default NotFound;
